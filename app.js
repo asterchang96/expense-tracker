db9316f2628b0dc3fe02e770d0d45d062a351d5c
asterchang96@gmail.com <asterchang96@gmail.com>
2021-07-27 09:39:39 +0800

    feat: add mongoose & models

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// setting static files
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
-    res.render('new')
+    res.render('show')
 })
 
+app.get('/new', (req, res) => {
+    res.render('new')
+})
 
 app.listen( PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
