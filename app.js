fba3bb85c675bf765f541c4bc7c85a3a23044e24
asterchang96@gmail.com <asterchang96@gmail.com>
2021-08-02 18:10:38 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static('public'))
 app.use(routes)
 
   
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
 })
