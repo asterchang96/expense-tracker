7fdf9ebb18e34bf5a7f4bc28ef46459684fb8f25
asterchang96@gmail.com <asterchang96@gmail.com>
2021-08-15 20:43:51 +0800

    modify date search range and categort function with conditional filiter to search'.

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const moment = require('moment')
 
 const Category = require('../../models/category') // 載入 category model
 const Record = require('../../models/record') // 載入 record model
@@ -13,22 +12,21 @@ router.get('/', async(req, res)=> {
   const userId = req.user._id
   /* console.log(moment(dateStart).isBetween(dateStart, dateEnd))  */
 
-  
-  if(!dateStart && !dateEnd) {//chooseCategory搜尋
-    return Record.find({ category : chooseCategory, userId })
-    .lean()
-    .sort({ date: 'desc'})
-    .then((records) => {
-      records.forEach((record) => {
-        if(record.incomeOrExpenses === '收入') totalAmount += record.amount
-        else totalAmount -= record.amount
-        record.iconClass = (categories.find(category => (category.category === record.category))).iconClass
-      })
-      res.render('index', { records, categories, totalAmount, chooseCategory, dateStart, dateEnd })
-    })  
-  }else if(dateStart && dateEnd){//chooseCategory、time搜尋
-    if(chooseCategory === 'all'){
-      return Record.find({ date: { $gte: (dateStart), $lte: (dateEnd) }, userId })
+  //建立過濾器
+  let conditionFilter = {}
+  conditionFilter.userId = userId
+  if(chooseCategory !== 'all'){
+    conditionFilter.category = chooseCategory
+  }
+  if(dateStart && dateEnd){
+    conditionFilter.date = { $gte: (dateStart), $lte: (dateEnd) }
+  }else if(dateStart && !dateEnd){
+    conditionFilter.date = { $gte: (dateStart) }
+  }else if(dateStart && !dateEnd){
+    conditionFilter.date = { $lte: (dateEnd) }
+  }
+
+  return Record.find(conditionFilter)
         .lean()
         .sort({ date: 'desc'})
         .then((records) => {
@@ -39,26 +37,6 @@ router.get('/', async(req, res)=> {
           })
           res.render('index', { records, categories, totalAmount, chooseCategory, dateStart, dateEnd })
         })  
-    }
-    return Record.find({ category : chooseCategory, date: { $gte: (dateStart), $lte: (dateEnd) }, userId })
-      .lean()
-      .sort({ date: 'desc'})
-      .then((records) => {
-        records.forEach((record) => {
-          if(record.incomeOrExpenses === '收入') totalAmount += record.amount
-          else totalAmount -= record.amount
-          record.iconClass = (categories.find(category => (category.category === record.category))).iconClass
-        })
-        res.render('index', { records, categories, totalAmount, chooseCategory, dateStart, dateEnd })
-      })  
-  }
-  /* else{
-    let error = true
-    res.render('index', { error, records, categories, totalAmount, chooseCategory, dateStart, dateEnd })
-  } */
-
-
-
 })
 
 module.exports = router
